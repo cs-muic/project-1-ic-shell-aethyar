f035a47aa6b87dd4ee24e4cc77230b7055f9d0c5
Aethyar <aethyar@gmail.com>
2023-05-11 12:48:12 +0700

    Improve echo cmd
    - include more libraries
    - remove extra space after printing

--- icsh.c
+++ icsh.c
@@ -4,6 +4,8 @@
  */
 
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 #define MAX_CMD_BUFFER 255
 
@@ -15,7 +17,7 @@ int main() {
         fgets(buffer, 255, stdin);
         // printf("you said: %s\n", buffer);
         if (strncmp(buffer, "echo ", 5) == 0) {
-            printf("%s\n", buffer + 5);
+            printf("%s", buffer + 5);
         }
     }
 }
